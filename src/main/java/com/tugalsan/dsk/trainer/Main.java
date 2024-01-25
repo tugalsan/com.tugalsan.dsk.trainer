88abbb1587450ba3bd6f1f87a5972df573e6aefd
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-01-26 01:47:58 +0300

    .

--- src/main/java/com/tugalsan/dsk/trainer/Main.java
+++ src/main/java/com/tugalsan/dsk/trainer/Main.java
@@ -148,6 +148,7 @@ public class Main extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 var w = new Main();
                 w.setVisible(true);
